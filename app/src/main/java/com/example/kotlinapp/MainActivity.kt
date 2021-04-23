223d680c9c723731a8b05a345dad150517dafdf4
Matthewm1234 <mondaym12@yahoo.com>
2021-04-23 20:31:15 +0100

    App completed

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var incrementButton : Button = findViewById(R.id.increment_button)
+        var decrementButton : Button = findViewById(R.id.decrement_button)
+        var textView : TextView  = findViewById(R.id.textView)
+
+        incrementButton.setOnClickListener {
+            var stringValue  = textView.text.toString()
+            var originalValue = Integer.parseInt(stringValue)
+            textView.text = Integer.toString(originalValue + 1)
+        }
+
+        decrementButton.setOnClickListener {
+            var stringValue  = textView.text.toString()
+            var originalValue = Integer.parseInt(stringValue)
+            textView.text = Integer.toString(originalValue - 1)
+        }
+
     }
 }
